1b252da5c41ba9fdadf261477e7da75115ea2c22
Justin Oakes <admin@oklasoftware.com>
2018-01-24 19:54:45 -0600

    Initial Commit

---
+++ machoreader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  machoreader
+//
+//  Created by Justin Oakes on 24/1/18.
+//  Copyright © 2018 Oklasoft LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ machoreader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  machoreader
+//
+//  Created by Justin Oakes on 24/1/18.
+//  Copyright © 2018 Oklasoft LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
